abb7fa13b3c323bfbe8dca4712978d4b180dea81
kishore <kishoreb@xpanxion.co.in>
2018-04-04 19:04:48 +0530

    Commited Day 3

---
+++ Day3/MyCarSolution/CarDriverApp/CarDriver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCarNS;
+using MyCarPremium;
+
+namespace CarDriverApp
+{
+    class CarDriver
+    {
+        static void Main(string[] args)
+        {
+            MyCar car1 = new MyCar();
+            MyPremiumCars pc = new MyPremiumCars();
+            car1.DisplayCarDetails();
+            MyCar car2 = new MyCar("model1", 5, 4, 200);
+            car2.DisplayCarDetails();
+            Console.ReadKey();
+        }
+
+        public void MyCarCreation()
+        {
+            MyCar car1 = new MyCar("model1", 5, 4, 200);
+            MyCar car2 = new MyCar("model2", 2, 2, 400);
+            MyCar car3 = new MyCar(5, 4, 200);
+        }
+    }
+}
---
+++ Day3/MyCarSolution/MyCar/MyCar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarNS
+{
+    public class MyCar
+    {
+        #region COnstructors
+        public MyCar() { }
+
+        public MyCar(int doors, int seats, int fuelCap)
+        {
+            noOfDoors = doors;
+            noOfSeats = seats;
+            FuelCapacity = fuelCap;
+            currentGear = 0;
+            model = "defaultModel";
+        }
+
+        public MyCar(string model, int doors, int seats, int fuelCap)
+        {
+            this.model = model;//mandatory to provide this pointer
+            this.noOfDoors = doors;
+            this.noOfSeats = seats;            
+            FuelCapacity = fuelCap;
+            currentGear = 0;
+        }
+        #endregion
+
+        #region Data Members - xyz
+        string model;
+        int noOfDoors;
+        int noOfSeats;
+        readonly int noOfGears;
+        int currentGear;
+        int FuelCapacity;
+        #endregion
+
+        #region Public Methods
+        
+        public void DisplayCarDetails()
+        {
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Door#: {noOfDoors}");
+            Console.WriteLine($"Seat#: {noOfSeats}");
+            Console.WriteLine($"Current Gear#: {currentGear}");
+            Console.WriteLine($"Fuel Capacity: {FuelCapacity}");
+        }
+        #endregion
+    }
+
+
+}
+
